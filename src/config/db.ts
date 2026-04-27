37a8bd9bf62ca5d7d47727510091cb8dcc727912
Ats32-coder <aadukubja@gmail.com>
2026-04-27 22:02:47 +0300

    Comments

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,24 +1,26 @@
-import sql from "mssql";
+import sql from "mssql"; // Toob sisse mssql paketi (sellega ühendume andmebaasiga)
 
-export const dbConfig: sql.config = {
-  user: "sa",
-  password: "123",
-  server: "localhost",
-  database: "ReactData",
+export const dbConfig: sql.config = { // Siin määrame andmebaasi ühenduse seaded
+  user: "sa", // kasutajanimi (nt SQL Serveri admin)
+  password: "123", // parool
+  server: "localhost", // kus andmebaas asub (siin samas arvutis)
+  database: "ReactData", // andmebaasi nimi
   options: {
-    encrypt: false,
-    trustServerCertificate: true
+    encrypt: false, // kas kasutada krüpteerimist (false = ei)
+    trustServerCertificate: true // usaldab serveri sertifikaati
   },
-  port: 1433
+  port: 1433 // SQL Serveri port
 };
 
-export const poolPromise = new sql.ConnectionPool(dbConfig)
-  .connect()
+export const poolPromise = new sql.ConnectionPool(dbConfig) // Loome ühenduse andmebaasiga
+  .connect() // proovib ühendust luua
   .then((pool) => {
+    // Kui õnnestub, siis näitab konsoolis sõnumit
     console.log("Ühendus MS SQL-iga loodud!");
-    return pool;
+    return pool; // tagastab ühenduse, et seda mujal kasutada
   })
   .catch((err) => {
+    // Kui tekib viga, siis näitab errorit
     console.error("Ühenduse viga:", err);
-    throw err;
+    throw err; // viskab vea edasi
   });
--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,14 @@ import express from "express";
 import cors from "cors";
 import itemsRoutes from "./routes/items";
 
-const app = express();
-const PORT = 4000;
+const app = express(); // Loome serveri
+const PORT = 4000; // Määrame pordi (kus server töötab)
 
-app.use(cors());
-app.use(express.json());
+app.use(cors()); // Lubame päringud teistelt lehtedelt
+app.use(express.json()); // Ütleme serverile, et ta oskab JSON andmeid lugeda
 
-app.use("/api/items", itemsRoutes);
+app.use("/api/items", itemsRoutes); //Kui keegi läheb /api/items aadressile, siis kasutatakse itemsRoutes faili koodi
 
-app.listen(PORT, () => {
-  console.log(`Server töötab aadressil http://localhost:${PORT}`);
-});
+app.listen(PORT, () => { //Server tööle
+  console.log(`Server töötab aadressil http://localhost:${PORT}`); // Näitame konsoolis, et server töötab
+}); 
